d91906441cbb6ec3971053770c60e1a1d03215be
Clayton Knapp <80793319+clayton-knapp@users.noreply.github.com>
2021-12-15 18:51:30 -0800

    Initial commit

---
+++ fetch-utils.js
@@ -0,0 +1,64 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function createGame(game){
+    const response = await client
+        .from('games')
+        .insert({ 
+            ...game, 
+            user_id: client.auth.user().id, 
+        })
+        .single();
+
+    return checkError(response);
+}
+
+
+export async function getGames() {
+    const response = await client
+        .select()
+        .match({ user_id: client.auth.user().id, });
+
+    return checkError(response);    
+}
+
+export async function getUser() {
+    return client.auth.session();
+}
+
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('../'); 
+}
+
+export async function redirectToGames() {
+    if (await getUser()) {
+        location.replace('./games');
+    }
+}
+
+export async function signupUser(email, password){
+    const response = await client.auth.signUp({ email, password });
+    
+    return checkError(response);
+}
+
+export async function signInUser(email, password){
+    const response = await client.auth.signIn({ email, password });
+
+    return checkError(response);
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '/';
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
